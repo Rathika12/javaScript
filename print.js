62515c1d743c316c6552e8fc4dc4ee593f7e5da2
Rathika12 <60001344+Rathika12@users.noreply.github.com>
2021-04-20 12:36:36 +0530

    Add files via upload

---
+++ print.js
@@ -0,0 +1,19 @@
+let num = 10;
+while(num>0){
+    console.log(num);
+    num--;
+}
+let pass = "pass123";
+if(pass.length>8){
+    console.log("Strong password");
+}
+else{
+    console.log("Weak Password");
+}
+let mobilenum = "909009011";
+if(mobilenum.length==10){
+   console.log("Correct Mobile Number");
+}
+else{
+    console.log("Incorrect Mobile Number");
+}
